42c8c4ddb38fa81c3ab6ab83fdd9fe0771ca6ddb
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2021-11-06 06:02:01 -0700

    general structure defined

--- schedule.c
+++ schedule.c
@@ -1,3 +1,14 @@
+void fcfs(int processes[],int n, int burstTime[], int arrivalTime[]){
+
+}
+
 int main(){
-    
+    int processes[5];
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    int burstTime[5];
+
+    int arrivaltime[5];
+
+    fcfs(processes,n,burstTime,arrivaltime);
 }
