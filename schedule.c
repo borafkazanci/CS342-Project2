6d8844ff1b75332d08cc434c6628fbf9e498a38e
Bora Fatih KAZANCI <58588084+borafkazanci@users.noreply.github.com>
2021-11-18 00:10:20 +0300

    RR without queue and arrival time

--- schedule.c
+++ schedule.c
@@ -47,10 +47,62 @@ void push(Node** head, int d, int p){
 	}
 }
 
-void rr(){
+void rr_waiting_time(int brstNoArr[], int arrvlTime[], int brstLenArr[], 
+	  int arrLen, int quantum, int turnAround[], int wait[]){
+  
+  int tempBrstLen[arrLen];
+  for (int i = 0; i < arrLen; i++) {
+    tempBrstLen[i] = brstLenArr[i];
+  }
 
+  // int timeSpan[arrLen];
+  // timeSpan[0] = arrvlTime[0];
+  // wait[0] = 0;
+
+  int t = 0;
+  while(true){
+    bool check = true;
+
+    for (int i = 0; i < arrLen; i++){
+      if (tempBrstLen[i] > 0) {
+        check = false;
+
+        if (tempBrstLen[i] > quantum) {
+          t += quantum;
+          tempBrstLen[i] -= quantum;
+        }
+        else {
+          t += tempBrstLen[i];
+          wait[i] = t - brstLenArr[i];
+          tempBrstLen[i] = 0;
+        }
+      }
+    }
+
+    if (check == true) break;
+  }
 }
-void srtf(int brstNoArr[],int arrLen, int brstLenArr[], int arrvlTime[], bool check[]){
+
+void rr(int brstNoArr[], int arrvlTime[], int brstLenArr[], int arrLen, int quantum){
+	int turnAround[arrLen];
+  int wait[arrLen];
+	int totalTurnAround = 0;
+
+	rr_waiting_time(brstNoArr, arrvlTime, brstLenArr, arrLen, quantum, turnAround, wait);
+	// turnaround time
+	for (int i = 0; i < arrLen; i++){
+		turnAround[i] = brstLenArr[i] + wait[i];
+	}
+
+  for (int i = 0; i < arrLen; i++){
+		totalTurnAround += turnAround[i];
+	}
+
+  double rrAvgTurnAround = (double) totalTurnAround /(double) arrLen;
+  printf("RR: %f \n", rrAvgTurnAround);
+}
+
+void srtf(int brstNoArr[], int arrvlTime[], int brstLenArr[], int arrLen, bool check[]){
 	int doneCount = 0;
 	int currentTime = 0;
 	int totalTA = 0;
@@ -68,34 +120,35 @@ void srtf(int brstNoArr[],int arrLen, int brstLenArr[], int arrvlTime[], bool ch
 				}	
 			}
 		}
-	if(head == NULL){
-		currentTime++;
-	}	
-	else{
-		Node* temp = pop(&head);
-		if(temp->priority != 1){
-			if(head == NULL){
-				head = newNode(temp->data,temp->priority-1);
-			}
-			else{
-				push(&head,temp->data,temp->priority-1);
-			}
+
+		if(head == NULL){
 			currentTime++;
-			free(temp);
 		}
 		else{
+			Node* temp = pop(&head);
+			if(temp->priority != 1){
+				if(head == NULL){
+					head = newNode(temp->data,temp->priority-1);
+				}
+				else{
+					push(&head,temp->data,temp->priority-1);
+				}
 			currentTime++;
-			totalTA = totalTA + (currentTime - temp->data);
 			free(temp);
-			doneCount++;
-
+			}
+			else{
+				currentTime++;
+				totalTA = totalTA + (currentTime - temp->data);
+				free(temp);
+				doneCount++;
+			}
 		}
 	}
-	}
 	double srtfAvgTurnAround = (double) totalTA / (double) arrLen;
-  	printf("SRTF: %d \n", (int) round(srtfAvgTurnAround));
+  printf("SRTF: %d \n", (int) round(srtfAvgTurnAround));
 }
-void sjf(int brstNoArr[],int arrLen, int brstLenArr[], int arrvlTime[], bool check[]){
+
+void sjf(int brstNoArr[], int arrvlTime[], int brstLenArr[], int arrLen, bool check[]){
 	int doneCount = 0;
 	int currentTime = 0;
 	int totalTA = 0;
@@ -113,21 +166,21 @@ void sjf(int brstNoArr[],int arrLen, int brstLenArr[], int arrvlTime[], bool che
 				}	
 			}
 		}
-	if(head == NULL){
-		currentTime++;
-	}	
-	else{
-		Node* temp = pop(&head);
-		currentTime = currentTime + temp->priority;
-		totalTA = totalTA + (currentTime - temp->data);
-		free(temp);
-		doneCount++;
+		if(head == NULL){
+			currentTime++;
+		}	
+		else{
+			Node* temp = pop(&head);
+			currentTime = currentTime + temp->priority;
+			totalTA = totalTA + (currentTime - temp->data);
+			free(temp);
+			doneCount++;
+		}
 	}
-	}
-
 	double sjfAvgTurnAround = (double) totalTA / (double) arrLen;
-  	printf("SJF: %d \n", (int) round(sjfAvgTurnAround));
+  printf("SJF: %d \n", (int) round(sjfAvgTurnAround));
 }
+
 void fcfs_waiting_time(int brstNoArr[], int arrLen, int brstLenArr[],int wait[],int turnAround[],int arrvlTime[]){
   int timeSpan[arrLen];
   timeSpan[0] = arrvlTime[0];
@@ -135,7 +188,6 @@ void fcfs_waiting_time(int brstNoArr[], int arrLen, int brstLenArr[],int wait[],
 
   for(int i = 1; i < arrLen; i++){
     timeSpan[i] = timeSpan[i-1] + brstLenArr[i-1];
-
     wait[i] = timeSpan[i] - arrvlTime[i];
 
     if(wait[i] < 0){
@@ -143,21 +195,22 @@ void fcfs_waiting_time(int brstNoArr[], int arrLen, int brstLenArr[],int wait[],
     }
   }
 }
-void fcfs(int brstNoArr[],int arrLen, int brstLenArr[], int arrvlTime[]){
+
+void fcfs(int brstNoArr[], int arrvlTime[], int brstLenArr[], int arrLen){
   int turnAround[arrLen];
   int wait[arrLen];
 
 	fcfs_waiting_time(brstNoArr,arrLen,brstLenArr,wait,turnAround,arrvlTime);
+	// turnaround time
 	for(int i = 0; i < arrLen; i++){
     turnAround[i] = brstLenArr[i] + wait[i];     
-    }
+  }
   int totalTurnAround = 0;
   for(int i = 0; i < arrLen; i++){
     totalTurnAround += turnAround[i];
   }
 
   double fcfsAvgTurnAround = (double) totalTurnAround / (double) arrLen;
-
   printf("FCFS: %d \n", (int) round(fcfsAvgTurnAround));
 }
 
@@ -209,16 +262,19 @@ int main(int argc, char** argv){
 	}
   fclose(sNewFile);
 
+	int quantum = atoi(argv[2]);
+
 	// control
 	printf("File Control, Herkes fileini atsin!\n");
 	for (int i = 0; i < arrLen; i++){
 		printf("Arr index: %d => Burst no: %d - Arrival T: %d - Burst Length: %d\n", 
 			i, brstNoArr[i], arrvlTime[i], brstLenArr[i]);		
 	}
-	printf("\n\n\n");
+	printf("%d\n\n\n", quantum);
 	
-	// FCFS
-  fcfs(brstNoArr, arrLen, brstLenArr, arrvlTime);
-  sjf(brstNoArr, arrLen, brstLenArr, arrvlTime, checkArr);
-  srtf(brstNoArr, arrLen, brstLenArr, arrvlTime, checkArr);
+	// Algorithm Calls
+  fcfs(brstNoArr, arrvlTime, brstLenArr, arrLen);
+  sjf(brstNoArr, arrvlTime, brstLenArr, arrLen, checkArr);
+  srtf(brstNoArr, arrvlTime, brstLenArr, arrLen, checkArr);
+	rr(brstNoArr, arrvlTime, brstLenArr, arrLen, quantum);
 }
